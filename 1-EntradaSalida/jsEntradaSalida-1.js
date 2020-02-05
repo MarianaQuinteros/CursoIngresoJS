d26a88e205c2a99364fa9ee1b1bc6f853167271c
alumno <alumno@utnfra.com.ar>
2020-02-05 12:21:38 -0300

    clase 1 fin

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,12 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+
+	var texto;
+
+	//asigno la variable texto
+	texto="esto funciona de maravillas";
+
+	alert (texto);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,11 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+    var texto;
+
+    texto=prompt("Ingrese Nombre");
+
+    alert(texto);
 	
 }
 
