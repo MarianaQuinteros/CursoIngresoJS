433e4bb30d10e4300df164d3f2450ce32185c324
alumno <alumno@utnfra.com.ar>
2020-02-29 12:34:05 -0300

    clase 2019 recup

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,6 +1,6 @@
 function mostrar()
 {
-
+/*
     /*Realizar el algoritmo que permita ingresar la marca 
     del producto, el peso el cual debe ser entre 1 y 100 (validar),
     la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera e informar al terminar el ingreso por document.write: 
@@ -9,7 +9,8 @@ b) La marca del producto más pesado
 c) La cantidad de productos que se conservan a menos de 0 grados. 
 d) El promedio del peso de todos los productos.	
 f) El peso máximo y el mínimo.*/
-
+ 
+    /*
 
     var respuesta = true;
 
@@ -108,6 +109,148 @@ f) El peso máximo y el mínimo.*/
     document.write ("El promedio del peso de todos los productos es: "+ promediopeso+"<br>");
     document.write  ("Peso Maximo: "+maximopeso+ "<br>"+"Peso minimo: " + minimopeso)
 
+*/
 
 
-}
+/*
+)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva */
+
+
+    var nombre;
+    var cantpersonas;
+    var cantdias;
+    var formadepago;
+    var respuesta=true;
+    var acumuladordias=0;
+    var maxpersonas;
+    var maxdias;
+    var primeravez=true;
+    var contadorefectivo=0;
+    var contadorqr=0;
+    var contadortarjeta=0;
+    var maxnombre;
+    var maxdiaspers;
+    var contadorreservas=0;
+    var promedio;
+    
+
+    while (respuesta) {
+
+        do {
+        nombre = prompt ("INGRESE NOMBRE DEL HUESPED");
+    
+        } while (nombre == "");
+
+        do {
+         cantpersonas = prompt("INGRESE CANTIDAD DE PERSONAS A HOSPEDARSE");
+         cantpersonas = parseInt(cantpersonas)
+        
+        } while (isNaN (cantpersonas) || cantpersonas<0);
+
+        do {
+            cantdias = prompt("INGRESE CANTIDAD DE DIAS DE ESTADIA");
+            cantdias = parseInt(cantdias);
+            acumuladordias += cantdias;
+        
+        } while (isNaN(cantdias)) ;
+
+        
+        do {
+            formadepago= prompt("INGRESE FORMA DE PAGO");        
+      
+        }while (!isNaN(formadepago) ||   formadepago != "efectivo" && formadepago !="qr" && formadepago != "tarjeta" );
+
+        //aca empieza el ejercicio
+        //cuento las cant de veces que se usa cada forma de pago
+       
+
+        /*if (formadepago== "efectivo"){
+            contadorefectivo++;
+        }else {
+            if (formadepago== "qr") {
+                contadorqr++;
+         }else {
+                contadortarjeta++;
+            }
+        }
+*/ //TAMBIEN SE SPUEDE HACER EN SWITCH
+        switch(formadepago) {
+            
+            case ("efectivo"):
+                contadorefectivo++;
+                break;
+            case ("qr"):
+                contadorqr++;
+                break;
+            case ("tarjeta"):
+                contadortarjeta++;
+                break;
+
+        }
+
+          //informar el huésped que trajo más personas en una sola reserva
+        if (primeravez) {
+            primeravez = false
+            maxpersonas = cantpersonas;
+            maxnombre = nombre;
+            maxdias = cantdias;
+            maxdiaspers=cantpersonas
+
+        }
+        if (cantpersonas>maxpersonas) {
+            maxpersonas=cantpersonas;
+            maxnombre=nombre;
+        }
+
+        //la cantidad de personas que se quedaron más días
+        
+        if (cantdias> maxdias) {
+            maxdias = cantdias;
+            maxdiaspers=cantpersonas;
+
+        }
+        contadorreservas++
+        respuesta= confirm("DESEA CONTINUAR?");
+
+                
+    }
+
+       
+    //el promedio de cantidad de días por reserva 
+    promedio= acumuladordias / contadorreservas
+
+     //la forma de pago más utilizada.
+
+    if (contadorefectivo > contadortarjeta && contadorefectivo >contadorqr) {
+            document.write ("La forma de pago mas utilizada es EFECTIVO"+"<br>");
+
+        }else {
+            if (contadortarjeta > contadorqr) {
+                document.write("La forma de pago mas utilizada es TARJETA"+"<br>"); 
+                } else{ 
+
+                    document.write ("La forma de pago mas utilizada es QR"+"<br>"); 
+                        
+                  }
+              }
+         
+                       
+                                  
+    
+    document.write ("el promedio de cantidad de días por reserva es:" + promedio+ "<br>") ;   
+    document.write (maxdiaspers+" personas quedaron el maximo de días que es: "+maxdias+"<br>");
+    document.write (maxnombre+" trajo mas personas en una sola reserva, fueron: "+ maxpersonas+ "<br>");  
+
+
+    
+ }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,124 @@
 function mostrar()
 {
+    /*Realizar el algoritmo que permita ingresar el nombre de un estudiante, 
+    la edad (validar) , el sexo (validar) y la nota del final (validar)
+hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+a) La cantidad de varones aprobados
+b) El promedio de notas de los menores de edad
+c) El promedio de notas de los adolescentes.
+d) El promedio de notas de los mayores
+f) El promedio de notas por sexo masculino y femenino
+*/
+    var nombre;
+    var edad;
+    var sexo;
+    var notafinal;
+    var respuesta=true;
+    var varonesaprobados=0;
+    var acumuladornotasmenores=0;
+    var acumuladornotasadolescentes=0;
+    var acumuladornotasmayores=0;
+    var acumuladorpromediof=0;
+    var acumuladorpromediom=0;
+    var cantfem=0;
+    var cantmasc=0;
+    var cantmayores=0;
+    var cantmenores=0;
+    var cantadolescentes=0;
+    var promediofem;
+    var promediomasc;
+    var promediomenores;
+    var promediomayores;
+    var promedioadolescentes;
+
+    while (respuesta) {
+
+        do {
+            nombre = prompt ("INGRESE NOMBRE");
+            
+        }while (!isNaN(nombre));
+
+        do {
+            edad = prompt("INGRESE EDAD");
+            edad=parseInt(edad);
+
+        }while (isNaN(edad) || edad<0);
+
+        do {
+            sexo = prompt ("INGRESE SEXO DEL ALUMNO (f o m)");
+            
+        }while (!isNaN(sexo) || sexo !="f" && sexo != "m");
+
+        do {
+            notafinal = prompt("INGRESE NOTA FINAL");
+            notafinal= parseInt(notafinal);
+            
+        } while (isNaN(notafinal) || notafinal<=0 || notafinal >10);
+                 
+         //termino validacion.
+
+         //a) La cantidad de varones aprobados
+
+        if (sexo == "M" && notafinal>6){
+             varonesaprobados++
+         }
+
+         //b) El promedio de notas de los menores de edad
+
+        if (edad<14) {
+             acumuladornotasmenores+=notafinal
+             cantmenores++;
+         } else {
+             if (edad>=18) {
+             //d) El promedio de notas de los mayores
+             acumuladornotasmayores+=notafinal;
+             cantmasc++;
+            } else {
+                //c) El promedio de notas de los adolescentes.
+                acumuladornotasadolescentes+=notafinal;
+                cantadolescentes++;
+                } 
+             }
+
+            //f) El promedio de notas por sexo masculino y femenino
+
+        if (sexo == "f") {
+            acumuladorpromediof+=notafinal;
+            cantfem++
+            }else {
+                acumuladorpromediom+=notafinal;
+                cantmasc++;
+                }
+            
+             
+            respuesta= confirm("DESEA CONTINUAR?")
+    
+    
+    }
+
+    //f) El promedio de notas por sexo masculino y femenino
+    
+    promediofem = acumuladorpromediof / cantfem;
+
+    promediomasc = acumuladorpromediom / cantmasc;
+
+    promediomenores = acumuladornotasmenores / cantmenores;
+
+    promedioadolescentes = acumuladornotasadolescentes / cantadolescentes;
+
+    promediomayores = acumuladornotasmayores / cantmayores;
+
+    //informar por document write
+
+    document.write ("La cantidad de varones aprobados es: "+varonesaprobados+"<br>");
+    document.write ("El promedio de notas de los menores de edad es: "+promediomenores+"<br>");
+    document.write ("El promedio de notas de los adolescentes es: "+ promedioadolescentes+"<br>");
+    document.write ("El promedio de notas de los mayores es: "+ promediomayores+"<br>");
+    document.write ("El promedio de notas por sexo masculino es: "+ promediomasc+ "<br>");
+    document.write ("El promedio de notas por sexo femenino es: "+promediofem+"<br>");
+
+
+
+
 
 }
